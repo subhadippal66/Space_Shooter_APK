2933edb8a719f0975d4d8da1f807c238b2353160
subhadippal66 <62802794+subhadippal66@users.noreply.github.com>
2020-09-12 08:55:03 +0530

    Touch_Control of player added

---
+++ Space_Shooter_APK/Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //configuration parameter
+    [SerializeField] float offsetY = 1f;
+
+    void Start()
+    {
+      
+    }
+
+   
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(Input.touchCount>0)
+        {
+            
+            Touch touch = Input.GetTouch(0);
+            Camera gameCamera = Camera.main;
+
+            var moveX = gameCamera.ScreenToWorldPoint(touch.position).x;
+            var moveY = gameCamera.ScreenToWorldPoint(touch.position).y + offsetY;
+            transform.position = new Vector2(moveX, moveY);
+           
+        }
+    }
+}
