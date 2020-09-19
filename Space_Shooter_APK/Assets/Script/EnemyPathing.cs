d1b3d099c9f42c272e8bd5caf88c4afc504b7502
subhadippal66 <62802794+subhadippal66@users.noreply.github.com>
2020-09-19 19:41:21 +0530

    Enemy Added on a path

---
+++ Space_Shooter_APK/Assets/Script/EnemyPathing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> wayPoints;
+    int wayPointIndex = 0;
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = wayPoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+    public void MoveEnemy()
+    {
+        if(wayPointIndex < wayPoints.Count)
+        {
+            var currentPos = transform.position;
+            var targetPos = wayPoints[wayPointIndex].transform.position;
+            var travelSpeed = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(currentPos, targetPos, travelSpeed);
+
+            if(transform.position == wayPoints[wayPointIndex].transform.position)
+            {
+                wayPointIndex++;
+            }
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
